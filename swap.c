bc207bc19838bc17789c7050c1909c2b471b21e5
rubix545 <abhimanyuag98@gmail.com>
2018-10-27 14:42:15 +0530

    Updated swap.c

---
+++ swap.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    int x=10;
+    int y=5;
+    printf("\n Values before swapping");
+    printf("\n X: %d\n Y:",x,y);
+     printf("\n Values after swapping");
+    printf("\n X: %d\n Y:",x,y);
+
+}
